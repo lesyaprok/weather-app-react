4f709aa81d11afa03517654841882085569d81b7
Lesya Kosilova <lesyaaprok@gmail.com>
2023-01-30 15:59:17 +0300

    feat: add an ability to close the sidebar by clicking anywhere outside it

--- src/features/SavedCities/SavedCities.jsx
+++ src/features/SavedCities/SavedCities.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./SavedCities.module.css";
 import CloseButton from "../../ui/molecules/CloseButton/CloseButton";
 import RemoveButton from "../../ui/molecules/RemoveButton/RemoveButton";
@@ -9,9 +9,18 @@ const SavedCities = ({
   removeCityFromSaved,
   changeLocation,
   isSidebarOpen,
-  closeSidebar,
+  setSidebar,
 }) => {
   const [saved, setSaved] = useState([]);
+  const nodeRef = useRef(null);
+  const handleClick = (e) => {
+    if (!nodeRef.current.contains(e.target)) setSidebar(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    return () => document.removeEventListener("mousedown", handleClick);
+  }, []);
 
   const list = (
     <ul className={styles.list}>
@@ -33,11 +42,12 @@ const SavedCities = ({
 
   return (
     <aside
+      ref={nodeRef}
       className={`${styles.sidebar} ${
         isSidebarOpen ? "translate-x-0" : "translate-x-full"
       } `}
     >
-      <CloseButton onClick={closeSidebar} />
+      <CloseButton onClick={() => setSidebar(false)} />
       <h2 className={styles.title}>Favorites</h2>
       <SearchInSaved setSaved={setSaved} savedCities={savedCities} />
       {savedCities.length ? (
